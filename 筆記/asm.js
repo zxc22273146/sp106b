a79692662a99d3c6feabbc0cec9c62139ba3a8c7
xji6 <aa22979313@gmail.com>
2018-06-21 16:03:59 +0800

    finish by jay

---
+++ 筆記/asm.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var c  = console;
+var file = process.argv[2];
+
+assemble(file+'.asm', file+'.hack');
+
+function assemble(asmFile, objFile) {
+  var asmText = fs.readFileSync(asmFile, "utf8");
+                          
+  var lines = asmText.split(/\s/);
+  var ss=JSON.stringify(lines, null, 2)
+  c.log(ss);
+  var x = lines.toString();
+ 
+  var n = x.length;
+  c.log("n=%d",n);
+  var count = 0;
+  var r = [];
+
+  c.log(ss[2]);
+
+  while (count < n) {
+    if (ss[count] == "D") {
+    r.push("find");
+    }
+    else
+    count = count+1;
+  }
+  c.log(r);
+}
