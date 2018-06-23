3f7e0a49e05909807dca33bde02e3d8ca994fb78
zxc22273146 <zxc22273146@gmail.com>
2018-06-23 16:51:20 +0800

    finish 2018/06/23

--- JavaScript/Assembler.js
+++ JavaScript/Assembler.js
@@ -1,8 +1,8 @@
-var fs = require("fs");
+var fs = require("fs"); // 讀檔
 var c  = console;
 var file = process.argv[2];
 
-var dtable = {
+var dtable = { // 表格內容改成字串，因為好處理
   ""   :'000',
   "M"  :'001',
   "D"  :'010',
@@ -81,24 +81,19 @@ var symTable = {
   "SCREEN":'16384'
 };
 
-var symTop = 16;
-var symlocation = 0;
+var newsym = 16; // 新的符號從位置16開始
+var sym = 0; // 記住新增幾個符號
 
-function addSymbol(symbol) { //增加變數
-  symTable[symbol] = symTop; //將新的變數放進 symTable 裡面
-  symTop ++; 
-}
+assemble(file+'.asm', file+'.hack'); // 開檔案
 
-assemble(file+'.asm', file+'.hack'); //開檔案
-
-function assemble(asmFile, objFile){ //主程式
+function assemble(asmFile, objFile){ // 主程式
   var asmText = fs.readFileSync(asmFile, "utf8");
                           
   var lines = asmText.split(/\r?\n/);
-  //var sss = JSON.stringify(lines, null, 2);
-  //c.log(sss); //這2行用途是印出全部內容，並以換行符號分隔
-  var x = lines.join(" "); //將文字檔用空白符號分隔
-  //c.log(x); //檢查用
+  // var sss = JSON.stringify(lines, null, 2);
+  // c.log(sss); // 這2行用途是印出全部內容
+  var x = lines.join(" "); // 將文字檔用空白符號分隔
+  // c.log(x); // 檢查用
   var count = 0;
 
   while(count<x.length){
@@ -112,26 +107,38 @@ function assemble(asmFile, objFile){ //主程式
       }
       insA(r);
     }
-    else if(x[count] == "A" || x[count] == "D" || x[count] == "M" || x[count] == "0"){
-      if(x[count+1] == "=" || x[count+1] == ";"){
+    else if(x[count] == "A" || x[count] == "D" || x[count] == "M"){
+      if(x[count+1] == "=" || x[count+2] == "=" || x[count+1] == ";"){
         r.push(x[count]);
-        r.push(x[count+1]); //"="符號
+        r.push(x[count+1]);
         count++;
         while(x[count+1] != " " && count<x.length){
           r.push(x[count+1]);
           count++;
         }
         insC(r);
+      }
+    }
+    else if(x[count] == "0"){ // 0;JMP指令
+      if(x[count+1] == "=" || x[count+1] == ";"){
+        r.push(x[count]);
+        r.push(x[count+1]);
+        count++;
+        while(x[count+1] != " " && count<x.length){
+          r.push(x[count+1]);
+          count++;
         }
+        insC(r);
+      }
     }
     else if(x[count] == "("){
       while(x[count+1] != ")"){
         r.push(x[count+1]);
         count++;
       }
-      sym(r);
+      r.length = 0;
     }
-    else if(x[count] == "/"){ //將註解消除
+    else if(x[count] == "/"){ // 將註解消除
       if(x[count+3] != " ")
         count = count+3;
         r.push(x[count])
@@ -143,52 +150,51 @@ function assemble(asmFile, objFile){ //主程式
     }
     count++;
   }
+  c.log(" ");
+  c.log("------符號表中新增了 "+sym+" 個符號------")
 }
 
-function insA(r){ //A指令
+function insA(r){ // A指令
   c.log(r.join(""))
-    var n = r.shift(); //移除陣列中的"@"符號
-    x1 = r.join(""); //陣列轉字串
-    x1 = parseInt(x1); //字串轉數字
+    var n = r.shift(); // 移除陣列中的"@"符號
+    x1 = r.join(""); // 陣列轉字串
+    x1 = parseInt(x1); // 字串轉數字
     if(isNaN(x1)){
       x1 = r.join("");
-      if(symTable[x1] === undefined){ //查symTable
-        symTable[x1] = symTop;
-        symTop++;
+      if(symTable[x1] === undefined){ // 查symTable
+        symTable[x1] = newsym;
+        newsym++;
+        sym++;
       }
       var v = symTable[x1];
-      v = parseInt(v); //字串轉數字
-      v = v.toString(2); //數字轉二進位字串
-      while(v.length < 16){ //補零用迴圈
+      v = parseInt(v); // 字串轉數字
+      v = v.toString(2); // 數字轉二進位字串
+      while(v.length < 16){ // 補零用迴圈
         v = "0" + v;
       }
       return c.log(v);
     }
     else
-    x1 = x1.toString(2); //數字轉二進位字串
-    while(x1.length < 16){ //補零用迴圈
+    x1 = x1.toString(2); // 數字轉二進位字串
+    while(x1.length < 16){ // 補零用迴圈
       x1 = "0" + x1;
     }
     return c.log(x1);
 }
 
-function insC(r){ //C指令
+function insC(r){ // C指令
   c.log(r.join(""));
-  if(r[1] == "="){
+  if(r[1] == "=" || r[2] == "="){
     r1 = r.join("");
-    r2 = r1.split("="); //用"="分隔結果並放入陣列
-    //c.log(r2); //檢查用
+    r2 = r1.split("="); // 用"="分隔結果並放入陣列
+    // c.log(r2); // 檢查用
     var x2 = "111" + ctable[r2[1]] + dtable[r2[0]] + "000";
     return c.log(x2);
   }
-  else if(r[1] == ";"){ //跳躍指令
+  else if(r[1] == ";"){ // 跳躍指令
     r1 = r.join("");
-    r2 = r1.split(";"); //用";"分隔結果並放入陣列
+    r2 = r1.split(";"); // 用";"分隔結果並放入陣列
     var x2 = "111" + ctable[r2[0]] + "000" + jtable[r2[1]];
     return c.log(x2);
   }
-}
-
-function sym(r) {
-  symlocation++;
 }
